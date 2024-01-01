3042602157ad62ea2341808a0bfcaf97b9b36577
0563 <0563166@gmail.com>
2024-01-01 22:18:11 +0400

    hw21Cache

--- L22-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ L22-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,31 +1,48 @@
 package ru.otus.cachehw;
 
-
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 public class MyCache<K, V> implements HwCache<K, V> {
-//Надо реализовать эти методы
+
+    private final Map<K, V> cache = new WeakHashMap<>();
+    private final CopyOnWriteArrayList<HwListener<K, V>> listeners = new CopyOnWriteArrayList<>();
 
     @Override
     public void put(K key, V value) {
-
+        cache.put(key, value);
+        notifyListeners(key, value, "put");
     }
 
     @Override
     public void remove(K key) {
-
+        V value = cache.remove(key);
+        notifyListeners(key, value, "remove");
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return cache.get(key);
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
+        listeners.remove(listener);
+    }
 
+    private void notifyListeners(K key, V value, String action) {
+        for (HwListener<K, V> listener : listeners) {
+            try {
+                listener.notify(key, value, action);
+            } catch (Exception e) {
+                // Обработка исключений при оповещении слушателей
+                e.printStackTrace();
+            }
+        }
     }
 }
