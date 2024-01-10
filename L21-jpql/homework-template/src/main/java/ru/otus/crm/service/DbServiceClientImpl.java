35cb06facccb335b14bc59f317040e11cbcd3a33
0563 <0563166@gmail.com>
2024-01-10 15:45:32 +0400

    commit 1

--- L21-jpql/homework-template/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
+++ L21-jpql/homework-template/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -3,7 +3,6 @@ package ru.otus.crm.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.cachehw.HwCache;
-import ru.otus.cachehw.HwListener;
 import ru.otus.core.repository.DataTemplate;
 import ru.otus.core.sessionmanager.TransactionManager;
 import ru.otus.crm.model.Client;
